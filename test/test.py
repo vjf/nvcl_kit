d621fe977234c199d0f66685f62359b4589db0f0
XXXXXXX <YYYYYY>
2019-11-16 08:32:30 +1100

    Added logid test

--- test/test.py
+++ test/test.py
@@ -23,70 +23,74 @@ class TestNVCLKit(unittest.TestCase):
 
     @unittest.mock.patch('nvcl_kit.WebFeatureService', autospec=True)
     def test_empty_wfs(self, mock_wfs):
-            wfs_obj = mock_wfs.return_value
-            wfs_obj.getfeature.return_value = Mock()
+        wfs_obj = mock_wfs.return_value
+        wfs_obj.getfeature.return_value = Mock()
 
-            # Test empty but valid response
-            with open('empty_wfs.txt') as fp:
-                wfs_resp_str = fp.readline()
-                wfs_obj.getfeature.return_value.read.return_value = wfs_resp_str
-                param_obj = self.setup_param_obj(MAX_BOREHOLES)
-                kit = NVCLKit(param_obj)
-                l = kit.get_boreholes_list()
-                self.assertEqual(l, [])
+        # Test empty but valid response
+        with open('empty_wfs.txt') as fp:
+            wfs_resp_str = fp.readline()
+            wfs_obj.getfeature.return_value.read.return_value = wfs_resp_str
+            param_obj = self.setup_param_obj(MAX_BOREHOLES)
+            kit = NVCLKit(param_obj)
+            l = kit.get_boreholes_list()
+            self.assertEqual(l, [])
 
 
     @unittest.mock.patch('nvcl_kit.WebFeatureService', autospec=True)
     def test_max_bh_wfs(self, mock_wfs):
-            wfs_obj = mock_wfs.return_value
-            wfs_obj.getfeature.return_value = Mock()
+        wfs_obj = mock_wfs.return_value
+        wfs_obj.getfeature.return_value = Mock()
 
-            # Test full response, maximum number of boreholes is enforced
-            with open('full_wfs3.txt') as fp:
-                wfs_resp_list = fp.readlines()
-                wfs_resp_str = ''.join(wfs_resp_list)
-                wfs_obj.getfeature.return_value.read.return_value = wfs_resp_str.rstrip('\n')
-                param_obj = self.setup_param_obj(MAX_BOREHOLES)
-                kit = NVCLKit(param_obj)
-                l = kit.get_boreholes_list()
-                self.assertEqual(len(l), MAX_BOREHOLES)
-                l = kit.get_nvcl_id_list()
-                self.assertEqual(len(l), MAX_BOREHOLES)
+        # Test full response, maximum number of boreholes is enforced
+        with open('full_wfs3.txt') as fp:
+            wfs_resp_list = fp.readlines()
+            wfs_resp_str = ''.join(wfs_resp_list)
+            wfs_obj.getfeature.return_value.read.return_value = wfs_resp_str.rstrip('\n')
+            param_obj = self.setup_param_obj(MAX_BOREHOLES)
+            kit = NVCLKit(param_obj)
+            l = kit.get_boreholes_list()
+            self.assertEqual(len(l), MAX_BOREHOLES)
+            l = kit.get_nvcl_id_list()
+            self.assertEqual(len(l), MAX_BOREHOLES)
 
 
     @unittest.mock.patch('nvcl_kit.WebFeatureService', autospec=True)
     def test_all_bh_wfs(self, mock_wfs):
+        wfs_obj = mock_wfs.return_value
+        wfs_obj.getfeature.return_value = Mock()
+
+        # Test full response, unlimited number of boreholes
+        with open('full_wfs3.txt') as fp:
+            wfs_resp_list = fp.readlines()
+            wfs_resp_str = ''.join(wfs_resp_list)
+            wfs_obj.getfeature.return_value.read.return_value = wfs_resp_str.rstrip('\n')
+            param_obj = self.setup_param_obj(0)
+            kit = NVCLKit(param_obj)
+            l = kit.get_boreholes_list()
+            self.assertEqual(len(l), 102)
+            l = kit.get_nvcl_id_list()
+            self.assertEqual(len(l), 102)
+   
+
+    def test_imagelog_data(self):
+        kit = None
+        with unittest.mock.patch('nvcl_kit.WebFeatureService') as mock_wfs:
             wfs_obj = mock_wfs.return_value
             wfs_obj.getfeature.return_value = Mock()
-
-            # Test full response, maximum number of boreholes is enforced
             with open('full_wfs3.txt') as fp:
                 wfs_resp_list = fp.readlines()
                 wfs_resp_str = ''.join(wfs_resp_list)
                 wfs_obj.getfeature.return_value.read.return_value = wfs_resp_str.rstrip('\n')
                 param_obj = self.setup_param_obj(0)
                 kit = NVCLKit(param_obj)
-                l = kit.get_boreholes_list()
-                self.assertEqual(len(l), 102)
-                l = kit.get_nvcl_id_list()
-                self.assertEqual(len(l), 102)
-   
-
-    @unittest.mock.patch('nvcl_kit.urllib', autospec=True)
-    def test_imagelog_data(self, mock_urllib):
-        request_obj = mock_urllib.return_value
-        request_obj.request.return_value = Mock()
-        request_obj.request.return_value.url_open.return_value = Mock()
-        self.assertEqual(0,0)
-        # TODO: Complete this
-        #with open('dataset_coll.txt') as fp:
-        #    ds_coll_resp_list = fp.readlines()
-        #    ds_coll_resp_str = ''.join(wfs_resp_list)
-        #    request_obj.request.return_value.url_open.return_value.read.return_value = ds_coll_resp_str 
-        #    param_obj = self.setup_param_obj(0)
-        #    kit = NVCLKit(param_obj)
-        #    nvcl_list = kit.get_nvcl_id_list()
-        #    image_data_list = kit.get_imagelog_data(nvcl_list[0])
+        with unittest.mock.patch('urllib.request.urlopen') as mock_request:
+            open_obj = mock_request.return_value
+            with open('dataset_coll.txt') as fp:
+                resp_list = fp.readlines()
+                resp_str = ''.join(resp_list)
+                open_obj.__enter__.return_value.read.return_value = resp_str 
+                image_data_list = kit.get_imagelog_data("blah")
+                self.assertTrue(len(image_data_list), 5)
         
     # TODO: Test receiving borehole data
 
