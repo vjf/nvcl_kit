375f7641f841f519760a3fe926cdc66d1dfd7f70
XXXXXXX <YYYYYY>
2019-11-19 10:09:27 +1100

    Add more exception tests

--- test/test.py
+++ test/test.py
@@ -27,7 +27,7 @@ class TestNVCLKit(unittest.TestCase):
         return param_obj
 
 
-    def _wfs_exception_tester(self, mock_wfs, excep, msg):
+    def wfs_exception_tester(self, mock_wfs, excep, msg):
         mock_wfs.side_effect = excep
         wfs_obj = mock_wfs.return_value
         wfs_obj.getfeature.return_value = Mock()
@@ -44,13 +44,13 @@ class TestNVCLKit(unittest.TestCase):
         #
         # Tests that can handle exceptions in WebFeatureService function
         #
-        self._wfs_exception_tester(mock_wfs, ServiceException, 'WFS error:')
-        self._wfs_exception_tester(mock_wfs, RequestException, 'Request error:')
-        self._wfs_exception_tester(mock_wfs, HTTPException, 'HTTP error code returned:')
-        self._wfs_exception_tester(mock_wfs, OSError, 'OS error:')
+        self.wfs_exception_tester(mock_wfs, ServiceException, 'WFS error:')
+        self.wfs_exception_tester(mock_wfs, RequestException, 'Request error:')
+        self.wfs_exception_tester(mock_wfs, HTTPException, 'HTTP error code returned:')
+        self.wfs_exception_tester(mock_wfs, OSError, 'OS Error:')
 
 
-    def _wfs_read_exception_tester(self, mock_wfs, excep, msg):
+    def wfs_read_exception_tester(self, mock_wfs, excep, msg):
         wfs_obj = mock_wfs.return_value
         wfs_obj.getfeature.return_value = Mock()
         wfs_obj.getfeature.return_value.read.side_effect = excep
@@ -68,7 +68,7 @@ class TestNVCLKit(unittest.TestCase):
         # Tests that can handle exceptions in getfeature's read() function
         #
         for excep in [Timeout, RequestException, HTTPException, ServiceException, OSError]:
-            self._wfs_read_exception_tester(mock_wfs, excep, 'WFS GetFeature failed')
+            self.wfs_read_exception_tester(mock_wfs, excep, 'WFS GetFeature failed')
 
 
     @unittest.mock.patch('nvcl_kit.WebFeatureService', autospec=True)
@@ -158,8 +158,24 @@ class TestNVCLKit(unittest.TestCase):
                 open_obj.__enter__.return_value.read.return_value = resp_str 
                 imagelog_data_list = kit.get_imagelog_data("blah")
                 self.assertEqual(len(imagelog_data_list), 5)
-        
 
+
+    def urllib_exception_tester(self, exc, fn, msg, params):
+        with unittest.mock.patch('urllib.request.urlopen') as mock_request:
+            open_obj = mock_request.return_value
+            open_obj.__enter__.return_value.read.side_effect = exc
+            open_obj.__enter__.return_value.read.return_value = '' 
+            with self.assertLogs('nvcl_kit', level='WARN') as nvcl_log:
+                imagelog_data_list = fn(**params)
+                self.assertIn(msg, nvcl_log.output[0])
+    
+
+    def test_imagelog_exception(self):
+        kit = self.setup_kit()
+        self.urllib_exception_tester(HTTPException, kit.get_imagelog_data, 'HTTP Error:', {'nvcl_id':'dummy-id'})
+        self.urllib_exception_tester(OSError, kit.get_imagelog_data, 'OS Error:', {'nvcl_id':'dummy-id'})
+
+        
     def test_profilometer_data(self):
         #
         # Test get_profilometer_data()
@@ -173,6 +189,12 @@ class TestNVCLKit(unittest.TestCase):
                 open_obj.__enter__.return_value.read.return_value = resp_str 
                 prof_data_list = kit.get_profilometer_data("blah")
                 self.assertEqual(len(prof_data_list), 1)
+
+
+    def test_profilometer_exception(self):
+        kit = self.setup_kit()
+        self.urllib_exception_tester(HTTPException, kit.get_profilometer_data, 'HTTP Error:', {'nvcl_id':'dummy-id'})
+        self.urllib_exception_tester(OSError, kit.get_profilometer_data, 'OS Error:', {'nvcl_id':'dummy-id'})
 
         
     def test_spectrallog_data(self):
@@ -190,6 +212,12 @@ class TestNVCLKit(unittest.TestCase):
                 self.assertEqual(len(spectral_data_list), 15)
 
 
+    def test_spectrallog_exception(self):
+        kit = self.setup_kit()
+        self.urllib_exception_tester(HTTPException, kit.get_spectrallog_data, 'HTTP Error:', {'nvcl_id':'dummy-id'})
+        self.urllib_exception_tester(OSError, kit.get_spectrallog_data, 'OS Error:', {'nvcl_id':'dummy-id'})
+
+
     def test_borehole_data(self):
         #
         # Test get_borehole_data()
@@ -203,7 +231,12 @@ class TestNVCLKit(unittest.TestCase):
                 open_obj.__enter__.return_value.read.return_value = bytes(resp_str, 'ascii') 
                 bh_data_list = kit.get_borehole_data("dummy-id", 10.0, "dummy-class")
                 self.assertEqual(len(bh_data_list), 28)
-    
+
+
+    def test_borehole_exception(self):
+        kit = self.setup_kit()
+        self.urllib_exception_tester(HTTPException, kit.get_borehole_data, 'HTTP Error:', {'log_id': 'dummy-logid', 'height_resol': 20, 'class_name': 'dummy-class'})
+        self.urllib_exception_tester(OSError, kit.get_borehole_data, 'OS Error:',  {'log_id': 'dummy-logid', 'height_resol': 20, 'class_name': 'dummy-class'})
 
 
 if __name__ == '__main__':
